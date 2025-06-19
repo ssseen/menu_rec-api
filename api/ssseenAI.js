90f6f7af66b6951dc6ba61ba644aa0fdcae4158d
박세연 <lily_1125@naver.com>
2025-06-20 07:03:45 +0900

    update cors settings

--- api/ssseenAI.js
+++ api/ssseenAI.js
@@ -4,8 +4,9 @@ dotenv.config();
 const ai = new GoogleGenAI({apiKey: process.env.GEMINI_API_KEY});
 
 export default async function handler(req, res){
+    const allowedOrigin = "https://ssseen.github.io"
     
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", allowedOrigin);
     res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
